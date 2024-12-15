63f7e19d1fd859ce305ec7d8b1a8b2c4d18dff35
Tai〽️ <taitran@sandiego.edu>
2024-12-14 22:39:31 -0800

    more jokes

--- jokes.js
+++ jokes.js
@@ -1,31 +1,49 @@
-let alternateJokes = true;
+let jokeIndex = 0;
 
 async function dadjokes() {
-    const jokesFromDad = await fetch("https://icanhazdadjoke.com/", {
+    const jokesFrom = await fetch("https://icanhazdadjoke.com/", {
         method: 'GET',
         headers: { 'Accept': 'application/json' }
     })
 
     const selectJoke = document.querySelector(".jokes-marquee");
-    const jokesFromDaddata = await jokesFromDad.json();
-    return selectJoke.innerHTML = `<p class="jokes-marquee__p">${jokesFromDaddata.joke}</p>`
+    const jokesFromData = await jokesFrom.json();
+    return selectJoke.innerHTML = `<p class="jokes-marquee__p">${jokesFromData.joke}</p>`
 }
 
 async function chuckNorrisJokes() {
-    const jokesFromNorris = await fetch("https://api.chucknorris.io/jokes/random");
-    const jokesFromNorrisData = await jokesFromNorris.json();
+    const jokesFrom = await fetch("https://api.chucknorris.io/jokes/random");
+    const jokesFromData = await jokesFrom.json();
     const selectJoke = document.querySelector(".jokes-marquee");
-    return selectJoke.innerHTML = `<p class="jokes-marquee__p">${jokesFromNorrisData.value}</p>`
+    return selectJoke.innerHTML = `<p class="jokes-marquee__p">${jokesFromData.value}</p>`
+}
+
+async function geekJokes() {
+    const jokesFrom = await fetch("https://geek-jokes.sameerkumar.website/api?format=json");
+    const jokesFromData = await jokesFrom.json();
+    const selectJoke = document.querySelector(".jokes-marquee");
+    return selectJoke.innerHTML = `<p class="jokes-marquee__p">${jokesFromData.joke}</p>`
+}
+
+async function techJokes() {
+    const jokesFrom = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+    const jokesFromData = await jokesFrom.json();
+    const selectJoke = document.querySelector(".jokes-marquee");
+    return selectJoke.innerHTML = `<p class="jokes-marquee__p">${jokesFromData.joke}</p>`
 }
 
 async function jokesDisplay() {
-    if (alternateJokes) {
+    if (jokeIndex === 0) {
         await dadjokes();
-    } else {
+    } else if (jokeIndex === 1) {
         await chuckNorrisJokes();
+    } else if (jokeIndex === 2) {
+        await geekJokes();
+    } else {
+        await techJokes();
     }
-    alternateJokes = !alternateJokes;
-
+    
+    jokeIndex += 1 % 4;
 }
 
 jokesDisplay();
